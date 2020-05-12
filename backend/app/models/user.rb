2206a9e0d447e4e418d8465ee3e49ff9887cdd64
ellisandrews <ellisandrews1@gmail.com>
2020-05-12 12:34:50 -0500

    User model setup

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ApplicationRecord
     has_many :songs
+
+    has_secure_password
+
+    before_save :downcase_name
+
+    def downcase_name
+        self.first_name = self.first_name.downcase
+        self.last_name = self.last_name.downcase
+    end
+
+    def titleize_first_name
+        self.first_name.titleize
+    end
+
+    def titleize_last_name
+        self.last_name.titleize
+    end
+
+    def full_name
+        "#{titleize_first_name} #{titleize_last_name}"
+    end
+
 end
