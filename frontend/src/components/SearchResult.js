5f8cf2394b89351b888222703ef4d6fbc0a4be04
ellisandrews <ellisandrews1@gmail.com>
2020-05-13 17:53:05 -0500

    Add alt text for album artwork img in search results

--- frontend/src/components/SearchResult.js
+++ frontend/src/components/SearchResult.js
@@ -4,11 +4,12 @@ import React from 'react'
 const SearchResult = props => {
 
   const { imgSrc, title, artist } = props
+  const displayText = `${title} - ${artist}`
 
   return (
     <div>
-      <img style={{maxHeight: 30, maxWidth: 30}} src={imgSrc}/>
-      <span style={{paddingLeft: 20}}>{title} - {artist}</span>
+      <img style={{maxHeight: 30, maxWidth: 30}} src={imgSrc} alt={`${displayText} album artwork`} />
+      <span style={{paddingLeft: 20}}>{displayText}</span>
     </div>
   )
 
