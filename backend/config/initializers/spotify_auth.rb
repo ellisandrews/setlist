1f76cb0cf1babc6d9a5dccd4eac9ee2cea6b1224
ellisandrews <ellisandrews1@gmail.com>
2020-05-13 13:17:13 -0500

    Add helpful logging for spotify auth request error on startup

--- backend/config/initializers/spotify_auth.rb
+++ backend/config/initializers/spotify_auth.rb
@@ -1,2 +1,9 @@
 # Authenticate for spotify API requests
-RSpotify.authenticate(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'])
+client_id = ENV['CLIENT_ID']
+client_secret = ENV['CLIENT_SECRET']
+
+if !client_id || !client_secret
+    puts 'ERROR: Must set Spotify `CLIENT_ID` and `CLIENT_SECRET` environment variables!'
+end
+
+RSpotify.authenticate(client_id, client_secret)
