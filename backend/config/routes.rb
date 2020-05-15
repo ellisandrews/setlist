ab36d920e023798625777d681e96d53d1933ee8a
ellisandrews <ellisandrews1@gmail.com>
2020-05-15 11:08:55 -0500

    Clean up routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
+      # See link for reason for this structure: https://guides.rubyonrails.org/routing.html#shallow-nesting
+      resources :users, only: [:index, :create, :show] do
+        resources :songs, only: [:index, :create]
+      end
+
+      resources :songs, only: [:show, :update, :destroy] do
+        resources :sections, only: [:index, :create]
+      end
+
       post '/signup', to: 'users#create'
       post '/login', to: 'sessions#create'
       get '/current_user', to: 'sessions#show'
