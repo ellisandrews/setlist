6d059d6a556c543f415d60713ee5005c9f243255
ellisandrews <ellisandrews1@gmail.com>
2020-05-17 14:11:27 -0500

    Add a section to the Song form

--- frontend/src/components/songs/SongForm.js
+++ frontend/src/components/songs/SongForm.js
@@ -1,16 +1,80 @@
 import React, { Component } from 'react'
 import { Button, Form, Row, Col } from 'react-bootstrap'
+import update from 'immutability-helper'
 import SongHeader from './SongHeader'
 
 
+const sectionFactory = (name = 'Fake Name', chords = 'Fake Chords', strumming = 'Fake Strumming') => ({ name, chords, strumming })
+
+
 class SongForm extends Component {
+  
+  constructor(props) {
+    super(props)
+
+    // Songs must have at least one section, so initialize with one
+    this.state = {
+      sections: [sectionFactory()]
+    }
+  }
+  
+  handleSectionChange = (event, index) => {
+    
+    const { name, value } = event.target
+    
+    // Update the state for the section at the correct index of the sections array. 
+    // Use immutability helper to simplify the update while not mutating the state.
+    this.setState(prevState => {
+      // Update the prevState's section key at the specified index 
+      return update(
+        prevState, 
+        { sections: { [index]: { [name]: {$set: value} } }}
+      )
+    })
+  }
+
+  renderSections = () => {
+    return this.state.sections.map((section, index) => {
+      return (
+        <div>
+          <h5>Section {index + 1}</h5>
+          <Form.Group as={Row}>
+            <Form.Label column sm={2}>Name</Form.Label>
+            <Col sm={10}>
+              <Form.Control name="name" type="text" value={section.name} onChange={event => this.handleSectionChange(event, index)}/>
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row}>
+            <Form.Label column sm={2}>Chords</Form.Label>
+            <Col sm={10}>
+              <Form.Control name="chords" type="text" value={section.chords} onChange={event => this.handleSectionChange(event, index)}/>
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row}>
+            <Form.Label column sm={2}>Strumming</Form.Label>
+            <Col sm={10}>
+              <Form.Control name="strumming" type="text" value={section.strumming} onChange={event => this.handleSectionChange(event, index)}/>
+            </Col>
+          </Form.Group>
+        </div>
+      )
+    })
+    
+  }
+
   render() {
+
+    // TODO: DELETE!
+    console.log(this.state)
+
     const { handleChange, handleSubmit, spotifyData } = this.props
     return (
       <>
         <SongHeader spotifyData={spotifyData} />
         
         <Form onSubmit={handleSubmit}>
+
+          <h3>General Info</h3>
 
           {/* Capo */}
           <Form.Group as={Row}>
@@ -32,8 +96,12 @@ class SongForm extends Component {
             </Form.Group>
           </fieldset>
 
+          <h3>Sections</h3>
+
           {/* Section(s) */}
-          
+          {this.renderSections()}
+
+          <h3>Notes</h3>
 
           {/* Notes */}
           <Form.Group as={Row}>
