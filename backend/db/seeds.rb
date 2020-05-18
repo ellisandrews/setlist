a310e4a5160ee92490dd3a223f6299f59d30b3b5
ellisandrews <ellisandrews1@gmail.com>
2020-05-18 11:30:43 -0500

    Add a song to the DB seeds file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -4,3 +4,31 @@ DatabaseCleaner.clean
 
 # Create a fake user
 user = User.create!(first_name: 'John', last_name: 'Doe', email: 'johndoe@fake.com', password: 'password')
+
+# Create a song
+songData = {
+    title: "Gravity",
+    artist: "John Mayer",
+    artwork_url: "https://i.scdn.co/image/ab67616d0000b2737af5fdc5ef048a68db62b85f",
+    spotify_id: "3SktMqZmo3M9zbB7oKMIF7",
+    capo: nil,
+    guitar_type: "Electric",
+    notes: "Put a little reverb on it. I'll post links to videos soon!",
+    user: user,
+    sections_attributes: [
+        {
+            name: "Intro/Chorus",
+            chords: "G C",
+            strumming: "D D DUD UDUDU",
+            display_order: 1
+        },
+        {
+            name: "Verse",
+            chords: "Am7 D7 Gm/Bb Ebmaj7 D7",
+            strumming: "",
+            display_order: 2,
+        }
+    ]
+}
+
+song = Song.create!(songData)
