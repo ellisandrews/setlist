7e38db9d8df1eb3ae31cc2c9e5acbb43e162fae1
ellisandrews <ellisandrews1@gmail.com>
2020-05-20 16:50:32 -0500

    Style the spotify search page

---
+++ frontend/src/components/search/SearchHeader.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Image, Container } from 'react-bootstrap'
+import spotifyIcon from './spotify_icon.png'
+
+
+const SearchHeader = () => {
+  return (
+    <div id="search-header">
+      <Container id="spotify-logo">
+        <Image fluid src={spotifyIcon} alt="spotify-logo" />
+      </Container>
+      <p>Search Spotify for a song to add to your repertoire.</p>
+    </div>
+  )
+}
+
+
+export default SearchHeader
