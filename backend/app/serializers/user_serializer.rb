308d52f3ac96c645ea5067814089e48aa3e04f63
ellisandrews <ellisandrews1@gmail.com>
2020-05-20 11:14:57 -0500

    Capitalize user names in serialization

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,3 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
     attributes :id, :first_name, :last_name, :email
+
+    def first_name
+        object.titleize_first_name
+    end
+
+    def last_name
+        object.titleize_last_name
+    end
+
 end
