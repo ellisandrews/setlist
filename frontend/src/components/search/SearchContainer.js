f927925aceab24a30d8bdfb0b2f24c29dc432d00
ellisandrews <ellisandrews1@gmail.com>
2020-05-23 14:17:14 -0500

    Active result working, start on selecting with up/down arrow keys

--- frontend/src/components/search/SearchContainer.js
+++ frontend/src/components/search/SearchContainer.js
@@ -13,7 +13,26 @@ class SearchContainer extends Component {
 
     this.state = {
       query: '',
-      results: []
+      results: [],
+      selectedIndex: 0,  // Index of results array that is currently selected
+    }
+  }
+
+  handleKeyPress = event => {
+
+    console.log(event)
+
+    const { results, selectedIndex } = this.state
+    
+    // Only handle arrow up or down keys, which will navigate the selected item of the results list
+    if (event.keyCode === 38 && selectedIndex > 0) {
+      this.setState(prevState => ({
+        selectedIndex: prevState.selectedIndex - 1
+      }))
+    } else if (event.keyCode === 40 && selectedIndex < results.length - 1) {
+      this.setState( prevState => ({
+        selectedIndex: prevState.selectedIndex + 1
+      }))
     }
   }
 
@@ -29,13 +48,17 @@ class SearchContainer extends Component {
     // If there is no query string, set the results back to empty array and return.
     if (!query) {
       this.setState({
-        results: []
+        results: [],
+        selectedIndex: 0
       })
       return
     }
 
     const success = results => {
-      this.setState({ results })
+      this.setState({
+        results,
+        selectedIndex: 0  // When the results change, set the selectedIndex back to 0
+      })
     }
 
     // Make a request to the backend, which hits the Spotify API and returns the results.
@@ -46,13 +69,13 @@ class SearchContainer extends Component {
   }
 
   render() {
-    const { query, results } = this.state
+    const { query, results, selectedIndex } = this.state
 
     return (
-      <Container id='search-container' className="border">
+      <Container id="search-container" className="border">
         <SearchHeader/>
-        <SearchForm query={query} handleChange={this.handleChange} />
-        <SearchResults results={results} handleSpotifyTrack={this.props.handleSpotifyTrack}/>
+        <SearchForm query={query} handleChange={this.handleChange}/>
+        <SearchResults results={results} selectedIndex={selectedIndex} handleSpotifyTrack={this.props.handleSpotifyTrack}/>
       </Container>
     )
   }
--- frontend/src/components/search/SearchResults.js
+++ frontend/src/components/search/SearchResults.js
@@ -8,9 +8,9 @@ class SearchResults extends Component {
 
   renderResults = () => {
 
-    const { results, handleSpotifyTrack } = this.props
+    const { results, selectedIndex, handleSpotifyTrack } = this.props
 
-    return results.map(result => {
+    return results.map( (result, index) => {
 
       // Extract the relevant data from the result object
       const artwork_url = result.album.images.find(image => image.height === 64).url
@@ -18,7 +18,7 @@ class SearchResults extends Component {
       const artist = result.artists[0].name
 
       return (
-        <ListGroup.Item action as='button' onClick={() => handleSpotifyTrack(result)} key={result.id}>
+        <ListGroup.Item active={index === selectedIndex} action as='button' onClick={() => handleSpotifyTrack(result)} key={result.id}>
           <SearchResult artwork_url={artwork_url} title={title} artist={artist} />
         </ListGroup.Item>
       )
