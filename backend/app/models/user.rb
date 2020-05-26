9586584085ed4ea9367a34d6594baf15b171c973
ellisandrews <ellisandrews1@gmail.com>
2020-05-26 15:33:53 -0500

    Require password of at least 8 characters

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
     # Validations
     validates :first_name, :last_name, :email, :password_confirmation, presence: true 
     validates :email, uniqueness: { case_sensitive: false }
+    validates :password, length: { minimum: 8 }
 
     # ActiveRecord callbacks
     before_save :downcase_name
