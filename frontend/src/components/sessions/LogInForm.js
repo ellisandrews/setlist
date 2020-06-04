481cf69297ca48c6aad53200ff73d65b1253a90a
ellisandrews <ellisandrews1@gmail.com>
2020-06-03 19:18:19 -0500

    Initial work on Formik and Yup for session forms

--- frontend/src/components/sessions/LogInForm.js
+++ frontend/src/components/sessions/LogInForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Formik } from 'formik'
 import { Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { Button, Container, Form } from 'react-bootstrap'
@@ -7,24 +8,7 @@ import { login } from '../../actions/sessions'
 
 class LogInForm extends Component {
   
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: '',
-      password: ''
-    }
-  }
-
-  handleInputChange = event => {
-    const { name, value } = event.target
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit = event => {
-    // Prevent default form submission
-    event.preventDefault()
+  handleSubmit = values => {
 
     // Grab `history` and `location` for managing login redirects, and the `login` action creator
     const { history, location, login } = this.props
@@ -35,28 +19,49 @@ class LogInForm extends Component {
 
     // Call the login action creator with a callback to send the user to the homepage after successful login
     login(
-      this.state,
+      values,
       () => history.replace(from)
     )
   }
   
   render() {
 
+    // TODO: Upon submission, dump server errors back here somewhere!!
+    // e.g. <Form.Control.Feedback type="invalid">{errors.general}</Form.Control.Feedback>
+
     return (
       <Container className="col-10 text-center">
         <h1 className="session-form-header">Log In</h1>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group>
-            <Form.Control required type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleInputChange}/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Control required type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleInputChange}/>
-          </Form.Group>
-          <Container className="mx-auto text-center">
-            <Button variant="dark" type="submit">Submit</Button>
-          </Container>
-        </Form>
+
+        {/* Begin form, controlled by Formik */}
+        <Formik 
+          initialValues={{ email: '', password: '' }}
+          onSubmit={this.handleSubmit}
+        >
+        {
+          ({
+            handleSubmit,
+            handleChange,
+            values,
+            errors
+          }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Control type="email" name="email" placeholder="Email" value={values.email} onChange={handleChange}/>
+              </Form.Group>
+              <Form.Group>
+                <Form.Control type="password" name="password" placeholder="Password" value={values.password} onChange={handleChange}/>
+              </Form.Group>
+              <Container className="mx-auto text-center">
+                <Button variant="dark" type="submit">Submit</Button>
+              </Container>
+            </Form>
+          )
+        }
+        </Formik>  
+        
         <p style={{paddingTop: 20, fontSize: "smaller"}}>Don't have an account? <Link style={{color: 'grey'}} to="/signup">Sign Up</Link></p>
+      
       </Container>
     )
   }
--- frontend/src/components/sessions/SignUpForm.js
+++ frontend/src/components/sessions/SignUpForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Formik } from 'formik'
+import { object, string } from 'yup'
 import { Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { Button, Container, Form } from 'react-bootstrap'
@@ -18,23 +20,13 @@ class SignUpForm extends Component {
     }
   }
 
-  handleInputChange = event => {
-    const { name, value } = event.target
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit = event => {
-    // Prevent default form submission
-    event.preventDefault()
-
+  handleSubmit = values => {
     // Grab `history` for managing login redirects, and the `sigup` action creator
     const { history, signup } = this.props
 
     // Call the signup action creator with a callback to send the user to the homepage after success
     signup(
-      this.state,
+      values,
       () => history.push('/')
     )
   }
@@ -44,26 +36,55 @@ class SignUpForm extends Component {
     return (
       <Container className="col-10 text-center my-3">
         <h1 className="session-form-header">Sign Up</h1>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group>
-            <Form.Control required type="text" name="first_name" placeholder="First Name" value={this.state.first_name} onChange={this.handleInputChange}/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Control required type="text" name="last_name" placeholder="Last Name" value={this.state.last_name} onChange={this.handleInputChange}/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Control required type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleInputChange}/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Control required type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleInputChange}/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Control required type="password" name="password_confirmation" placeholder="Password Confirmation" value={this.state.password_confirmation} onChange={this.handleInputChange}/>
-          </Form.Group>
-          <Container className="mx-auto text-center">
-            <Button variant="dark" type="submit">Submit</Button>
-          </Container>
-        </Form>
+        
+        <Formik
+          initialValues={{first_name: '', last_name: '', email: '', password: '', password_confirmation: ''}}
+          validationSchema={object({
+            first_name: string().required('Required'),
+            last_name: string().required('Required'),
+            email: string().required('Required').email('Invalid email address'),
+            password: string().required('Required').min(8, 'Must be at least 8 characters'),
+            password_confirmation: string().required('Required').min(8, 'Must be at least 8 characters')
+          })}
+          onSubmit={this.handleSubmit}
+        >
+        {
+          ({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            errors,
+            touched
+          }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Control required type="text" name="first_name" placeholder="First Name" value={values.first_name} onChange={handleChange} onBlur={handleBlur} isInvalid={touched.first_name && !!errors.first_name}/>
+                <Form.Control.Feedback type="invalid">{errors.first_name}</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group>
+                <Form.Control required type="text" name="last_name" placeholder="Last Name" value={values.last_name} onChange={handleChange} onBlur={handleBlur} isInvalid={touched.last_name && !!errors.last_name}/>
+                <Form.Control.Feedback type="invalid">{errors.last_name}</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group>
+                <Form.Control required type="email" name="email" placeholder="Email" value={values.email} onChange={handleChange} onBlur={handleBlur} isInvalid={touched.email && !!errors.email}/>
+                <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group>
+                <Form.Control required type="password" name="password" placeholder="Password" value={values.password} onChange={handleChange} onBlur={handleBlur} isInvalid={touched.password && !!errors.password}/>
+                <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group>
+                <Form.Control required type="password" name="password_confirmation" placeholder="Password Confirmation" value={values.password_confirmation} onChange={handleChange} onBlur={handleBlur} isInvalid={touched.password_confirmation && !!errors.password_confirmation}/>
+                <Form.Control.Feedback type="invalid">{errors.password_confirmation}</Form.Control.Feedback>
+              </Form.Group>
+              <Container className="mx-auto text-center">
+                <Button variant="dark" type="submit">Submit</Button>
+              </Container>
+            </Form>
+          )
+        }
+        </Formik>
         <p style={{paddingTop: 20, fontSize: "smaller"}}>Already have an account? <Link style={{color: 'grey'}} to="/login">Log In</Link></p>
       </Container>
     )
