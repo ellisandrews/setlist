cd030063026ec5462124850610109b650a67ec2a
ellisandrews <ellisandrews1@gmail.com>
2020-06-08 14:00:05 -0500

    Serialize song sections in order, update seed_data

--- backend/app/serializers/song_serializer.rb
+++ backend/app/serializers/song_serializer.rb
@@ -1,5 +1,7 @@
 class SongSerializer < ActiveModel::Serializer
     attributes :id, :guitar_type, :capo, :strumming, :notes, :youtube_id, :user_id
     belongs_to :spotify_track
-    has_many :sections
+    has_many :sections do
+        object.sections.order(:display_order)
+    end
 end
